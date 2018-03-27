990a503eb216d6b557d6454068eea7e528796051
pollockcl <pollock.clee@gmail.com>
2018-03-27 16:31:45 -0600

    got test built for students view page

--- spec/features/courses/can_see_courses_student_is_taking_spec.rb
+++ spec/features/courses/can_see_courses_student_is_taking_spec.rb
@@ -4,7 +4,11 @@ describe 'visit the student show page' do
   describe 'sees' do
     scenario 'all the courses the student is taking' do
       student = Student.create(name: 'Anakin Skywalker')
-      course = Course.create(name: 'Jedi Academy')
+      course = student.courses.create(name: 'Jedi Academy')
+
+      visit student_path(student)
+
+      expect(page).to have_content(course.name)
     end
   end
 end
