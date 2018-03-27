517d487a20fb540fbf79d975375be70e8bddf61a
pollockcl <pollock.clee@gmail.com>
2018-03-27 15:08:21 -0600

    added base model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327203026) do
+ActiveRecord::Schema.define(version: 20180327210447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20180327203026) do
     t.datetime "updated_at", null: false
     t.bigint "student_id"
     t.index ["student_id"], name: "index_addresses_on_student_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
