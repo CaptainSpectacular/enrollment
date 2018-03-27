f537df440a7850ab73e2f71c55e04b01f3d3aeb5
pollockcl <pollock.clee@gmail.com>
2018-03-27 14:31:35 -0600

    added optional relationship ;

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,5 @@ class Address < ApplicationRecord
   validates :state, presence: true
   validates :street, presence: true
   validates :zip, presence: true
+  belongs_to :student, optional: true
 end
