2418f32232e5c2036f5b4f66785c78395fdd1286
pollockcl <pollock.clee@gmail.com>
2018-03-27 15:03:18 -0600

    tests passing

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,24 @@
 class AddressesController < ApplicationController
+  before_action :set_student, only: %i[new create]
+
   def new
-    render locals: { student: Student.find(params[:student_id]),
+    render locals: { student: @student,
                      address: Address.new }
   end
 
   def create
-    Address.create(address_params)
+    @student.addresses.create(address_params)
 
     redirect_to student_path(params[:student_id])
   end
 
   private
 
+  def set_student
+    @student = Student.find(params[:student_id])
+  end
+
   def address_params
-    params.require(:address).permit(:description, :city, :state, :zip, :street, :student_id)
+    params.require(:address).permit(:description, :city, :state, :zip, :street)
   end
 end
