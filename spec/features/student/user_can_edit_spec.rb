667fbaeaf25d2f2f4bcb45cf57626912ba5f0d59
pollockcl <pollock.clee@gmail.com>
2018-03-27 13:42:52 -0600

    added base test for student editi

---
+++ spec/features/student/user_can_edit_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'user visits' do
+  describe 'the edit page' do
+    scenario 'they can edit a student' do
+      student = Student.create(name: 'Anakin Skywalker')
+
+      visit edit_student_path(student)
+
+      fill_in 'student[name]', with: 'Darth Vader'
+      click_on 'Edit Student'
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content('Darth Vader')
+    end
+  end
+end
