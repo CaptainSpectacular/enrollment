872f423325618bee17b530f2b6382ac765bd472b
pollockcl <pollock.clee@gmail.com>
2018-03-27 14:22:27 -0600

    added tests for the address model

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Address do
+  describe 'validations' do
+    it 'must have a city' do
+      address = Address.create(description: 'Sand everywhere',
+                               street: 'Sandy Drive',
+                               state: 'TA',
+                               zip: 90543)
+
+      expect(address).to_not be_valid
+    end
+
+    it 'must have a zip' do
+
+      address = Address.create(description: 'Sand everywhere',
+                               street: 'Sandy Drive',
+                               state: 'TA',
+                               city: 'Tatooine')
+
+      expect(address).to_not be_valid
+    end
+
+    it 'must have a street' do
+      address = Address.create(description: 'Sand everywhere',
+                               state: 'TA',
+                               city: 'Tatooine',
+                               zip: 90543)
+
+      expect(address).to_not be_valid
+    end
+
+    it 'must have a state' do
+      address = Address.create(description: 'Sand everywhere',
+                               city: 'Tatooine',
+                               street: 'Sandy Drive',
+                               zip: 90543)
+
+      expect(address).to_not be_valid
+    end
+
+    it 'is valid with all attributes' do
+      address = Address.create(description: 'Sand everywhere',
+                               street: 'Sandy Drive',
+                               city: 'Tatooine',
+                               state: 'TA',
+                               zip: 90543)
+
+      expect(address).to be_valid
+    end
+  end
+end
