efa9dcc662097f883d2656c44d476f26296f7178
pollockcl <pollock.clee@gmail.com>
2018-03-27 16:26:34 -0600

    fixed issue with faulty migration

--- db/migrate/20180327212646_add_course_to_student_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseToStudentCourses < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :courses, :student_courses, foreign_key: true
-  end
-end
--- db/migrate/20180327212719_add_student_to_student_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentToStudentCourses < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :students, :student_courses, foreign_key: true
-  end
-end
